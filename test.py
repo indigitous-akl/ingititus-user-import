9ac5c496907e689b8a49300d1c3391babea3d224
Thomas Phillips <thomas@teknique.com>
2019-09-14 11:16:51 +1200

    Initial commit

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from gremlin_python import statics
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.strategies import *
+from gremlin_python.process.traversal import Barrier
+from gremlin_python.process.traversal import Bindings
+from gremlin_python.process.traversal import Cardinality
+from gremlin_python.process.traversal import Column
+from gremlin_python.process.traversal import Direction
+from gremlin_python.process.traversal import Operator
+from gremlin_python.process.traversal import Order
+from gremlin_python.process.traversal import P
+from gremlin_python.process.traversal import Pop
+from gremlin_python.process.traversal import Scope
+from gremlin_python.process.traversal import T
+from gremlin_python.process.traversal import WithOptions
+
+remote_connection = DriverRemoteConnection('ws://localhost:8182/gremlin','g')
+g = traversal().withRemote(remote_connection)
+print("Vertices: {}".format(g.V().toList()))
+remote_connection.close()
